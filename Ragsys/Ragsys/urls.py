4c43f8ce411f195ab046f547c7759ff89ee88a6f
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-08-26 12:21:00 +0530

    successfull routing urls & views

--- Ragsys/Ragsys/urls.py
+++ Ragsys/Ragsys/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from baseapp.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('baseapp.urls')),
+    
 ]
--- Ragsys/baseapp/views.py
+++ Ragsys/baseapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from baseapp.models import *
+from . import views
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'index.html')
