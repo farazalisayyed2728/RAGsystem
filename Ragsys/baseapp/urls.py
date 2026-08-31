020706a7a856c5d7bc97d833babc8e1d8e3d413c
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-08-31 20:23:08 +0530

    urls routing

--- Ragsys/baseapp/urls.py
+++ Ragsys/baseapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('documentation/', views.documentation, name='documentation'),
-    
+    path('contact/', views.contact, name='contact'),
 ]
